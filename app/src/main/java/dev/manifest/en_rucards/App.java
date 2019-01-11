d78733977240420c3222e8027325544d9faf5aa9
manifest <doslovno@gmail.com>
2019-01-12 00:52:29 +0300

    add App

---
+++ app/src/main/java/dev/manifest/en_rucards/App.java
@@ -0,0 +1,34 @@
+package dev.manifest.en_rucards;
+
+
+import android.app.Application;
+
+import dev.manifest.en_rucards.di.component.AppComponent;
+import dev.manifest.en_rucards.di.component.DaggerAppComponent;
+import dev.manifest.en_rucards.di.module.AppModule;
+import dev.manifest.en_rucards.di.module.NetModule;
+
+public class App extends Application {
+
+    private AppComponent appComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Dagger%COMPONENT_NAME%
+        appComponent = DaggerAppComponent.builder()
+                // list of modules that are part of this component need to be created here too
+                .appModule(new AppModule(this)) // This also corresponds to the name of your module: %component_name%Module
+                .netModule(new NetModule())
+                .build();
+
+        // If a Dagger 2 component does not have any constructor arguments for any of its modules,
+        // then we can use .create() as a shortcut instead:
+        //  mAppComponent = com.codepath.dagger.components.DaggerAppComponent.create();
+    }
+
+    public AppComponent getAppComponent() {
+        return appComponent;
+    }
+}
