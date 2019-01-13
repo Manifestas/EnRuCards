983c3b76a9402b4a0112077cf3e9137effcd508b
tv.unimol <doslovno@gmail.com>
2019-01-13 12:46:20 +0300

    fix not checking token expire time

--- app/src/main/java/dev/manifest/en_rucards/network/LingvoTokenManager.java
+++ app/src/main/java/dev/manifest/en_rucards/network/LingvoTokenManager.java
@@ -1,6 +1,7 @@
 package dev.manifest.en_rucards.network;
 
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import java.io.IOException;
 import java.util.Calendar;
@@ -14,6 +15,8 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class LingvoTokenManager implements TokenManager {
+    
+    private static final String TAG = LingvoTokenManager.class.getSimpleName();
 
     public static final String ACCESS_TOKEN = "accessToken";
     public static final String TOKEN_EXPIRE_TIME = "tokenExpireTime";
@@ -31,14 +34,23 @@ public class LingvoTokenManager implements TokenManager {
 
     public LingvoTokenManager() {
         sharedPreferences = App.getAppComponent().getSharedPreference();
+        retrofit = App.getAppComponent().getRetrofit();
     }
 
     @Override
     public String getToken() {
         if (!hasToken()) {
+            Log.d(TAG, "has no token.");
             token = sharedPreferences.getString(ACCESS_TOKEN, null);
             //get expire time from shared preferences
             expireTime = sharedPreferences.getLong(TOKEN_EXPIRE_TIME, 0);
+            //if token not saved in preferences.
+            if (token == null) {
+                Log.d(TAG, "getToken: token not saved in sharedPreference.");
+                //refresh token here , and got new access token
+                token = refreshToken();
+            }
+        } else {
             Calendar calendar = Calendar.getInstance();
             Date nowDate = calendar.getTime();
 
@@ -46,9 +58,10 @@ public class LingvoTokenManager implements TokenManager {
             Date expireDate = calendar.getTime();
 
             int result = nowDate.compareTo(expireDate);
-        /* if token not saved in preferences its null. When comparing dates -1 means date passed
-         so we need to refresh token see {@link Date#compareTo} */
-            if (token == null || result < 0) {
+            /* When comparing dates -1 means date passed
+            so we need to refresh token see {@link Date#compareTo} */
+            if (result < 0) {
+                Log.d(TAG, "getToken: token expired.");
                 //refresh token here , and got new access token
                 token = refreshToken();
             }
@@ -64,6 +77,7 @@ public class LingvoTokenManager implements TokenManager {
     @Override
     public void clearToken() {
         token = null;
+        expireTime = 0;
         saveTokenToPreference(null, 0);
     }
 
@@ -71,12 +85,15 @@ public class LingvoTokenManager implements TokenManager {
     public String refreshToken() {
         Response<String> tokenResponse;
         try {
+            Log.d(TAG, "refreshToken: get new token from api.");
             tokenResponse = retrofit.create(LingvoApi.class).getAuthToken().execute();
         } catch (IOException e) {
+            Log.d(TAG, "refreshToken: " + e);
             return null;
         }
         String newAccessToken = tokenResponse.body();
         if (tokenResponse.isSuccessful() && newAccessToken != null) {
+            Log.d(TAG, "refreshToken: request successful.");
             token = newAccessToken;
             Calendar calendar = Calendar.getInstance();
             calendar.add(Calendar.DATE, DAYS_BEFORE_TOKEN_EXPIRES);
@@ -87,6 +104,7 @@ public class LingvoTokenManager implements TokenManager {
     }
 
     private void saveTokenToPreference(String token, long expireTime) {
+        Log.d(TAG, "saveTokenToPreference: token = " + token + "\nexpireTIme = " + expireTime);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(ACCESS_TOKEN, token);
         editor.putLong(TOKEN_EXPIRE_TIME, expireTime);
