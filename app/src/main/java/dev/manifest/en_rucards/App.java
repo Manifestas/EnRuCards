2e4d4815d27f7970498fded371baedf8618705ef
tv.unimol <doslovno@gmail.com>
2019-01-17 18:46:22 +0300

    remove db building from App

--- app/src/main/java/dev/manifest/en_rucards/App.java
+++ app/src/main/java/dev/manifest/en_rucards/App.java
@@ -3,8 +3,6 @@ package dev.manifest.en_rucards;
 
 import android.app.Application;
 
-import androidx.room.Room;
-import dev.manifest.en_rucards.data.db.CardDatabase;
 import dev.manifest.en_rucards.di.component.AppComponent;
 import dev.manifest.en_rucards.di.component.DaggerAppComponent;
 import dev.manifest.en_rucards.di.module.AppModule;
@@ -12,7 +10,6 @@ import dev.manifest.en_rucards.di.module.AppModule;
 public class App extends Application {
 
     private static AppComponent appComponent;
-    CardDatabase cardDatabase;
 
     public static AppComponent getAppComponent() {
         return appComponent;
@@ -31,8 +28,5 @@ public class App extends Application {
         // If a Dagger 2 component does not have any constructor arguments for any of its modules,
         // then we can use .create() as a shortcut instead:
         //  mAppComponent = com.codepath.dagger.components.DaggerAppComponent.create();
-        cardDatabase = Room.databaseBuilder(this, CardDatabase.class, CardDatabase.DB_NAME)
-                .fallbackToDestructiveMigration()
-                .build();
     }
 }
