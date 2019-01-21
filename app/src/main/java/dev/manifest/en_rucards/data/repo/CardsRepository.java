42d90417703450fbc3183c43515e2184aa0b5f94
tv.unimol <doslovno@gmail.com>
2019-01-21 21:37:40 +0300

    implement getFile in CardsRepository

--- app/src/main/java/dev/manifest/en_rucards/data/repo/CardsRepository.java
+++ app/src/main/java/dev/manifest/en_rucards/data/repo/CardsRepository.java
@@ -1,25 +1,30 @@
 package dev.manifest.en_rucards.data.repo;
 
+import java.io.InputStream;
 import java.util.List;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.inject.Singleton;
 
 import androidx.annotation.NonNull;
 import dev.manifest.en_rucards.data.model.Card;
+import dev.manifest.en_rucards.data.storage.FileStorage;
+import dev.manifest.en_rucards.data.storage.SoundFileStorage;
 
 @Singleton
 public class CardsRepository implements CardsDataSource {
 
     private CardsDataSource localDataSource;
     private CardsDataSource remoteDataSource;
+    private FileStorage fileStorage;
 
     @Inject
     public CardsRepository(CardsLocalDataSource localDataSource,
-                           CardsRemoteDataSource remoteDataSource) {
+                           CardsRemoteDataSource remoteDataSource,
+                           FileStorage fileStorage) {
         this.localDataSource = localDataSource;
         this.remoteDataSource = remoteDataSource;
+        this.fileStorage = fileStorage;
     }
 
     @Override
@@ -79,5 +84,36 @@ public class CardsRepository implements CardsDataSource {
     @Override
     public void deleteCard(@NonNull String cardId) {
 
+    }
+
+    @Override
+    public void getFile(@NonNull String fileName, @NonNull GetFileCallback callback) {
+        localDataSource.getFile(fileName, new GetFileCallback() {
+            @Override
+            public void onFileLoaded(InputStream inputStream) {
+                if (inputStream != null) {
+                    callback.onFileLoaded(inputStream);
+                } else {
+                    // request from internet
+                    remoteDataSource.getFile(fileName, new GetFileCallback() {
+                        @Override
+                        public void onFileLoaded(InputStream inputStream) {
+                            fileStorage.saveFile(SoundFileStorage.DIR_NAME, fileName, inputStream);
+                            callback.onFileLoaded(fileStorage.getFile(SoundFileStorage.DIR_NAME, fileName));
+                        }
+
+                        @Override
+                        public void onFileNotAvailable() {
+                            callback.onFileNotAvailable();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFileNotAvailable() {
+                callback.onFileNotAvailable();
+            }
+        });
     }
 }
