ea01daa6e63a3c3afb0500842f8a0857850ed677
manifest <doslovno@gmail.com>
2019-01-22 23:59:34 +0300

    decode inputStream from Base64

--- app/src/main/java/dev/manifest/en_rucards/data/storage/SoundFileStorage.java
+++ app/src/main/java/dev/manifest/en_rucards/data/storage/SoundFileStorage.java
@@ -1,6 +1,8 @@
 package dev.manifest.en_rucards.data.storage;
 
 import android.content.Context;
+import android.util.Base64;
+import android.util.Base64InputStream;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -32,22 +34,17 @@ public class SoundFileStorage implements FileStorage {
     public boolean saveFile(String fileName, InputStream inputStream) {
         File directory = context.getDir(DIR_NAME, Context.MODE_PRIVATE);
         File soundFile = new File(directory, fileName);
-        try {
-            try (FileOutputStream outputStream = new FileOutputStream(soundFile)) {
-                byte[] buffer = new byte[4096];
-                int bytesRead;
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
-                }
-                outputStream.flush();
-                return true;
-            } catch (IOException e) {
-                return false;
-            } finally {
-                if (inputStream != null) {
-                    inputStream.close();
-                }
+        try (FileOutputStream outputStream = new FileOutputStream(soundFile);
+             // response is Base64 file, decode it
+             Base64InputStream base64InputStream = new Base64InputStream(inputStream, Base64.DEFAULT)) {
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = base64InputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
             }
+            outputStream.flush();
+            return true;
         } catch (IOException e) {
             return false;
         }
