43d5dc2fab2c53fa03165b85cfcc0bd73667de2e
tv.unimol <doslovno@gmail.com>
2019-01-24 12:07:49 +0300

    change CardsDataSource for using rx

--- app/src/main/java/dev/manifest/en_rucards/data/repo/CardsDataSource.java
+++ app/src/main/java/dev/manifest/en_rucards/data/repo/CardsDataSource.java
@@ -1,45 +1,23 @@
 package dev.manifest.en_rucards.data.repo;
 
-import java.io.InputStream;
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import dev.manifest.en_rucards.data.model.Card;
+import io.reactivex.Flowable;
 
 public interface CardsDataSource {
 
-    void getCards(@NonNull LoadCardCallback callback);
+    Flowable<List<Card>> getCards();
 
-    void getCard(@NonNull String cardId, @NonNull GetCardCallback callback);
+    Flowable<Card> getCard(@NonNull String cardId);
 
-    void getCardByOriginalWord(@NonNull String word, @NonNull GetCardCallback callback);
+    Flowable<Card> getCardByOriginalWord(@NonNull String word);
 
     void saveCard(@NonNull Card card);
 
     void deleteCard(@NonNull String cardId);
 
-    void getFile(@NonNull Card card, @NonNull GetFileCallback callback);
-
-
-    interface LoadCardCallback {
-
-        void onCardsLoaded(List<Card> cards);
-
-        void onDataNotAvailable();
-    }
-
-
-    interface GetCardCallback {
-
-        void onCardLoaded(Card card);
-
-        void onDataNotAvailable();
-    }
-
-    interface GetFileCallback {
-
-        void onFileLoaded(String path);
-
-        void onFileNotAvailable();
-    }
+    Flowable<String> getFile(@NonNull Card card);
 }
+
