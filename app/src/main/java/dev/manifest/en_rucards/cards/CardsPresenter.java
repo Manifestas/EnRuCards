d9ba7eaa5cb1efb1a9d8f93a752cd64e52582742
manifest <doslovno@gmail.com>
2019-01-27 12:40:27 +0300

    add clear disposable

--- app/src/main/java/dev/manifest/en_rucards/cards/CardsPresenter.java
+++ app/src/main/java/dev/manifest/en_rucards/cards/CardsPresenter.java
@@ -100,6 +100,7 @@ public class CardsPresenter implements CardsContract.Presenter {
 
     @Override
     public void dropView() {
+        subscriptions.clear();
         wordsView = null;
     }
 }
